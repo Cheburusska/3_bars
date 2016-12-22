ee1c04f5ea310ae5d8c4c83fe1968510fef45555
Cheburusska <deminvictor@gmail.com>
2016-12-22 14:21:41 +0300

    mistakes fixed 2
    renamed uninformative names: data; name.

--- bars.py
+++ bars.py
@@ -11,35 +11,35 @@ def load_data(filepath):
         return json.load(json_file)
 
 
-def get_biggest_bar(data):
+def get_biggest_bar(data_from_json ):
     '''returns biggest bar'''
-    seats = data[0]['Cells']['SeatsCount']
-    name = ''
-    for bar in data:
+    seats = data_from_json [0]['Cells']['SeatsCount']
+    bar_name = ''
+    for bar in data_from_json :
         if bar['Cells']['SeatsCount'] > seats:
             seats = bar['Cells']['SeatsCount']
-            name = bar['Cells']['Name']
-    return name, seats
+            bar_name = bar['Cells']['Name']
+    return bar_name, seats
 
 
-def get_smallest_bar(data):
+def get_smallest_bar(data_from_json ):
     '''returns smallest bar'''
-    seats = data[0]['Cells']['SeatsCount']
-    name = data[0]['Cells']['Name']
-    for bar in data:
+    seats = data_from_json [0]['Cells']['SeatsCount']
+    bar_name = data_from_json [0]['Cells']['Name']
+    for bar in data_from_json :
         if bar['Cells']['SeatsCount'] < seats:
             seats = bar['Cells']['SeatsCount']
-            name = bar['Cells']['Name']
-    return name, seats
+            bar_name = bar['Cells']['Name']
+    return bar_name, seats
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(data_from_json , longitude, latitude):
     '''returns closest bar'''
-    initial_latitude = data[0]['Cells']['geoData']['coordinates'][1]
-    initial_longitude = data[0]['Cells']['geoData']['coordinates'][0]
-    name = data[0]['Cells']['Name']
+    initial_latitude = data_from_json [0]['Cells']['geoData']['coordinates'][1]
+    initial_longitude = data_from_json [0]['Cells']['geoData']['coordinates'][0]
+    bar_name = data_from_json [0]['Cells']['Name']
     initial_distance = abs(((latitude - initial_latitude) ** 2 + (longitude - initial_longitude) ** 2) ** 0.5)
-    for bar in data:
+    for bar in data_from_json :
         bar_latitude = bar['Cells']['geoData']['coordinates'][1]
         bar_longitude = bar['Cells']['geoData']['coordinates'][0]
         bar_distance = ((latitude - bar_latitude) ** 2 + (longitude - bar_longitude) ** 2) ** 0.5
@@ -47,9 +47,9 @@ def get_closest_bar(data, longitude, latitude):
             initial_latitude = bar['Cells']['geoData']['coordinates'][1]
             initial_longitude = bar['Cells']['geoData']['coordinates'][0]
             initial_distance = bar_distance
-            name = bar['Cells']['Name']
+            bar_name = bar['Cells']['Name']
             address = bar['Cells']['Address']
-    return name, initial_latitude, initial_longitude, address
+    return bar_name, initial_latitude, initial_longitude, address
 
 
 if __name__ == '__main__':
@@ -59,15 +59,15 @@ if __name__ == '__main__':
 
     while True:
         json_path = input("Укажите путь к существующему файлу json: ")
-        data = load_data(json_path)
-        if data is not None:
+        data_from_json = load_data(json_path)
+        if data_from_json is not None:
             break
 
-    print('Название самого большого бара: "%s"' % get_biggest_bar(data)[0])
-    print('Количество мест в нем: %s' % get_biggest_bar(data)[1])
+    print('Название самого большого бара: "%s"' % get_biggest_bar(data_from_json )[0])
+    print('Количество мест в нем: %s' % get_biggest_bar(data_from_json )[1])
 
-    print('Название самого маленького бара: "%s"' % get_smallest_bar(data)[0])
-    print('Количество мест в нем: %s' % get_smallest_bar(data)[1])
+    print('Название самого маленького бара: "%s"' % get_smallest_bar(data_from_json )[0])
+    print('Количество мест в нем: %s' % get_smallest_bar(data_from_json )[1])
 
     while True:
         print('Чтобы найти ближайший бар, введите свои координаты:')
@@ -76,10 +76,10 @@ if __name__ == '__main__':
         if longitude.isdigit() == True and latitude.isdigit() == True:
             longitude = float(longitude)
             latitude = float(latitude)
-            print('Название самого близкого бара: "%s"' % get_closest_bar(data, longitude, latitude)[0])
-            print('Его координаты: широта %s, долгота %s' % (get_closest_bar(data, longitude, latitude)[1],
-                  get_closest_bar(data, longitude, latitude)[2]))
-            print('Адрес: %s' % get_closest_bar(data, longitude, latitude)[3])
+            print('Название самого близкого бара: "%s"' % get_closest_bar(data_from_json , longitude, latitude)[0])
+            print('Его координаты: широта %s, долгота %s' % (get_closest_bar(data_from_json , longitude, latitude)[1],
+                  get_closest_bar(data_from_json , longitude, latitude)[2]))
+            print('Адрес: %s' % get_closest_bar(data_from_json , longitude, latitude)[3])
             break
         else:
             print("Необходимо вводить только числа")
